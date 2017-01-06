5c8424a952a72886accbd35c2a73b94d6e43466e
Risteski Petar <bobpetar@gmail.com>
2017-01-06 20:51:15 +0100

    role model, roles to users registrated and admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+['registered', 'admin'].each do |role|
+  Role.find_or_create_by({name: role})
+end
+
+User.create!(email:"email@example.com", password:"password", password_confirmation:"password")
+admin = User.create!(email:"admin@example.com", password:"password", password_confirmation:"password")
+admin.role.name = 'admin'
