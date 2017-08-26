ada6c014385d495722ce35794ff74b89e6984500
Petar Risteski <bobpetar@gmail.com>
2017-08-26 14:08:15 +0200

    fix datatable/team generator table UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,5 @@
 //= require progressive_render
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
-//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require turbolinks
 //= require_tree .
