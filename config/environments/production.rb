30695b20c41cee1c134013c9d06bd77747bc3739
Petar Risteski <bobpetar@gmail.com>
2017-09-07 15:43:29 +0200

    update s3 production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    :s3_host_name => 'apigateway.eu-west-2.amazonaws.com',
+    :s3_host_name => 'eu-west-2.amazonaws.com',
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
