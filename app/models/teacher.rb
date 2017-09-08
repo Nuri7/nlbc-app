928c02d3d790d4e43eb08a84f5c3d7240da21edb
Petar Risteski <bobpetar@gmail.com>
2017-09-08 15:08:15 +0200

    bugfix: filtering trainers

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -5,18 +5,19 @@ class Teacher < ApplicationRecord
   def self.search_trainers(params)
     trainers = Teacher.all
 
+
     if params['challenges'].present?
-      trainers = trainers.where(challenge_id: params['challenge_id'])
+      trainers = trainers.where(challenge_id: params['challenges'])
     end
 
     if params['locations'].present?
       bootcamps = Bootcamp.where(location: params['locations']).map{|bo| bo.challenges.map(&:id).compact.uniq}.flatten
-      trainers = trainers.where(id: bootcamps)
+      trainers = trainers.where(challenge_id: bootcamps)
     end
 
     if params['passions'].present?
-      passions = Bootcamp.where(passion_id: params['passions']).map{|bo| bo.challenges.map(&:id).compact.uniq}.flatten
-      trainers = trainers.where(id: passions)
+      passions = Passion.where(id: params['passions']).map{|bo| bo.challenges.map(&:id).compact.uniq}.flatten
+      trainers = trainers.where(challenge_id: passions)
     end
     trainers = User.where(id: trainers.collect(&:user_id).compact.uniq)
     trainers
