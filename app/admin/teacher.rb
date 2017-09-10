f0f26d2e962b46cd65f1f6a9ee03b1f83881f3e7
Petar Risteski <bobpetar@gmail.com>
2017-09-10 20:40:02 +0200

    order users dropdown

--- app/admin/teacher.rb
+++ app/admin/teacher.rb
@@ -17,7 +17,7 @@ permit_params :user_id, :challenge_id
 form do |f|
   f.semantic_errors
   f.inputs do
-    f.input :user_id, :as => :select, :collection => User.all.map{|u| ["#{u.email} - #{u.last_name}, #{u.first_name}", u.id]}
+    f.input :user_id, :as => :select, :collection => User.order(:email).map{|u| ["#{u.email} - #{u.last_name} #{u.first_name}", u.id]}
     f.input :challenge_id, :as => :select, :collection => Challenge.order(:title).map{|u| ["#{u.title} - Passion: #{u.passion.title}", u.id]}
   end
   f.submit
