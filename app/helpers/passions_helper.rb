60de7792efeffee1acfba7aba220222869e4d291
Petar Risteski <bobpetar@gmail.com>
2017-09-16 16:40:49 +0200

    fix download challenge link

--- app/helpers/passions_helper.rb
+++ app/helpers/passions_helper.rb
@@ -1,9 +1,9 @@
 module PassionsHelper
   def can_download?(challenge_id)
-    if UserChallenge.where(user_id: current_user.id, challenge_id: challenge_id).any?
+    if current_user and UserChallenge.where(user_id: current_user.id, challenge_id: challenge_id).any?
       true
     else
       false
-    end 
+    end
   end
 end
