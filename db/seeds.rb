5a28fd92d4b0fa74ed5b883b336551d39c44c094
Petar Risteski <bobpetar@gmail.com>
2017-09-27 01:07:48 +0200

    seed fake users and teachers

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,76 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(email:"email@example.com", password:"password", password_confirmation:"password")
-User.create!(email:"admin@example.com", password:"password", password_confirmation:"password", role: 'admin')
-User.create!(email:"nuri@nlbc.com", password:"password", password_confirmation:"password", role: 'admin')
-1.upto(10) do |i|
-  User.create(email:"test_#{i}@test-email.com",password:"password",password_confirmation:"password")
+["Pedro	Valdez",
+"Joshua	Burns",
+"Ricky	Welch",
+"Courtney	Erickson",
+"Horace	Tate",
+"Nicholas	Hawkins",
+"Bobby	Cruz",
+"Vernon	Morris",
+"Ronnie	Diaz",
+"Dan	Baker",
+"Marlon	Howard",
+"Malcolm	Lynch",
+"Dominic	Simpson",
+"Travis	Conner",
+"Marcus	Cole",
+"Ricardo	Stewart",
+"Elias	Fields",
+"Tracy	Mclaughlin",
+"Enrique	Craig",
+"Rick	Blair",
+"Nick	Hunt",
+"Craig	Wilson",
+"Sean	Mcdonald",
+"Bryan	Ramos",
+"Jose	Sullivan",
+"Clifford	Walton",
+"Hector	Hughes",
+"Kurt	Elliott",
+"Clark	Hunter",
+"Mike	Christensen",
+"Orville	Wolfe",
+"Nathan	Vasquez",
+"Clayton	Roy",
+"Guy	Bates",
+"Damon	Kelly",
+"Fredrick	Mack",
+"Rogelio	Hodges",
+"Alex	Moody",
+"Jerome	Hicks",
+"Tommy	Hansen",
+"Clint	Francis",
+"Jeffery	Blake",
+"Francis	Henderson",
+"Keith	Warner",
+"Carl	Little",
+"Amos	Evans",
+"Jerry	Gray",
+"Forrest	Ingram",
+"Curtis	Day",
+"Micheal	Ferguson"].each do |name|
+  user = User.create(
+    email:"#{name.split(' ').join}@nlbc.com",
+    password:"password",
+    password_confirmation:"password",
+    male: true,
+    first_name: name.split(' ').first,
+    last_name: name.split(' ').last,
+    passion: Passion.order("RANDOM()").first.title,
+    city: ["Eindhoven", "Amsterdam", "Rotterdam", "London", "Manchester"].sample
+  )
+  user.avatar = URI.parse("https://s3-eu-west-1.amazonaws.com/fakepeople/male/256/#{rand(1..58)}.png")
+  user.save
 end
 
-["QA", "Developer", "Teacher"].each do |passion|
-  name = passion.downcase
-  Passion.create(title: name)
+Passion.all.each do |passion|
+  users = User.order("RANDOM()").limit(10)
+  challenges = passion.challenges
+  challenges.each do |challenge|
+    users.each do |user|
+      Teacher.create(user: user, challenge: challenge)
+    end
+  end
 end
-
-["Java_and_BDD1", "Java_and_BDD2", "Performance_testing", "Mobile_testing", "Javascript_and_Protractor"].each do |bootcamp|
-  name = bootcamp.downcase
-  Bootcamp.create(title: name)
-end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
