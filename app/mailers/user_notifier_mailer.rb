753deaba3b132c672a7b8a914832f06af44b9163
Petar Risteski <bobpetar@gmail.com>
2017-09-28 12:37:51 +0200

    mail template

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -3,6 +3,21 @@ class UserNotifierMailer < ApplicationMailer
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_trainer_email(trainer, challenges)
+    headers "X-SMTPAPI" => {
+     "sub": {
+       "%name%" => [trainer.first_name trainer.last_name],
+       "%challenges%" => [challenges.map{|challenge| challenge.title }.join(', ')]
+     },
+     "filters": {
+       "templates": {
+         "settings": {
+           "enable": 1,
+           "template_id": '7ecac38b-b21e-417f-8e0b-4e6c239f98ac'
+         }
+       }
+     }
+    }.to_json
+
     @user = trainer
     @challenges = challenges
     mail( :to => @user.email,
